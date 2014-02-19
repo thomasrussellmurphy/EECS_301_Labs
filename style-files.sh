904710f1be51b3c79af13e90cd8fb279f4c99407
Thomas Russell Murphy <thomas.russell.murphy@case.edu>
2014-02-19 09:20:09 -0500

    Changed style script to just hold the default args to avoid having to reformat all top level verilog files due to comment-in-declaration bug.

--- style-files.sh
+++ style-files.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Style all verilog files in project directory and user's V file
-iStyle --indent=tab --style=kr --suffix=".old~" --pad=all */*.v */V/*.v
+iStyle --indent=tab --style=kr --suffix=".old~" --pad=all $@
