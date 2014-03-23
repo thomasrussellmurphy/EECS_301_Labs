2b5734b29c1cb72d945e88ebe081afe768c59bf8
Thomas Russell Murphy <thomas.russell.murphy@case.edu>
2014-03-23 18:50:24 -0400

    Added clock timing requirement for 50MHz input clock and added blanking based on pixel position for diagnostics.

--- lab5/V/character_display.v
+++ lab5/V/character_display.v
@@ -9,18 +9,37 @@ output wire [ 7: 0 ] disp_red, disp_green, disp_blue;
 output wire disp_clk, disp_en, disp_vsync, disp_hsync;
 
 wire [ 9: 0 ] horz_pos, vert_pos; // 10-bit draw position
-wire [ 7: 0 ] red_int, green_int, blue_int; // Internal 8-bit color values for the vga_controller
+reg [ 7: 0 ] red_int, green_int, blue_int; // Internal 8-bit color values for the vga_controller
 
 assign disp_en = en;
 
-assign red_int = horz_pos[ 7: 0 ];
-assign green_int = vert_pos[ 7: 0 ];
-assign blue_int = 8'h8e;
+wire valid_draw;
+reg [ 7: 0 ] next_red_int, next_green_int, next_blue_int;
 
 pll9mhz disp_clk_pll ( .inclk0( clk ), .c0( disp_clk ), .locked() );
 
 vga_controller controller ( .clk( clk ), .disp_clk( disp_clk ), .en( en ), .red_in( red_int ), .green_in( green_int ), .blue_in( blue_int ),
-                            .valid_draw(), .h_pos( horz_pos ), .v_pos( vert_pos ),
+                            .valid_draw( valid_draw ), .h_pos( horz_pos ), .v_pos( vert_pos ),
                             .red_out( disp_red ), .green_out( disp_green ), .blue_out( disp_blue ),
                             .disp_hsync( disp_hsync ), .disp_vsync( disp_vsync ) );
+
+always @( * ) begin
+    if ( valid_draw ) begin
+        next_red_int <= horz_pos[ 7: 0 ];
+        next_green_int <= vert_pos[ 7: 0 ];
+        next_blue_int <= 8'h8e;
+    end
+    else begin
+        next_red_int <= 1'b0;
+        next_green_int <= 1'b0;
+        next_blue_int <= 1'b0;
+    end
+end
+
+always @( posedge disp_clk ) begin
+    red_int <= next_red_int;
+    green_int <= next_green_int;
+    blue_int <= next_blue_int;
+end
+
 endmodule
