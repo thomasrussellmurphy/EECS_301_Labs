6ad92b5addaefbdbdddb10f12ff57d0051de8c70
Thomas Russell Murphy <thomas.russell.murphy@case.edu>
2014-03-25 08:59:53 -0400

    Formatted lab5 files.

--- lab5/V/RomTable.v
+++ lab5/V/RomTable.v
@@ -1,34 +1,32 @@
 module RomTable(
-cellAddress,
-horizDisp,
-vertDisp,
-clk,
-enable,
-data
-);
-input [3:0] cellAddress;
-input [2:0] horizDisp, vertDisp;
-input clk; 
+           cellAddress,
+           horizDisp,
+           vertDisp,
+           clk,
+           enable,
+           data
+       );
+input [ 3: 0 ] cellAddress;
+input [ 2: 0 ] horizDisp, vertDisp;
+input clk;
 input enable;
 output reg data;
 
-reg[63:0] rom [15:0];
-reg[5:0] bitIndex;
-reg[63:0] charCell;
+reg[ 63: 0 ] rom [ 15: 0 ];
+reg[ 5: 0 ] bitIndex;
+reg[ 63: 0 ] charCell;
 
 initial begin
-	$readmemb("ROM_INIT.txt", rom);
+    $readmemb( "ROM_INIT.txt", rom );
 end
 
-always @(posedge clk)
-begin
-if (enable)
-begin
-	bitIndex[2:0] <= horizDisp;
-	bitIndex[5:3] <= vertDisp;
-	charCell = rom[cellAddress];
-	data = charCell[bitIndex];
-end
+always @( posedge clk ) begin
+    if ( enable ) begin
+        bitIndex[ 2: 0 ] <= horizDisp;
+        bitIndex[ 5: 3 ] <= vertDisp;
+        charCell = rom[ cellAddress ];
+        data = charCell[ bitIndex ];
+    end
 end
 endmodule
 
--- lab5/V/StorageWriter.v
+++ lab5/V/StorageWriter.v
@@ -1,54 +1,54 @@
 module StorageWriter(
-input[3:0] newChar,
-input[5:0] address,
-input CLK,
-input VSync,
-output reg[11:0] Ram_Address,
-output reg[3:0] Ram_Data,
-output reg Ram_Enable
-);
+           input[ 3: 0 ] newChar,
+           input[ 5: 0 ] address,
+           input CLK,
+           input VSync,
+           output reg[ 11: 0 ] Ram_Address,
+           output reg[ 3: 0 ] Ram_Data,
+           output reg Ram_Enable
+       );
 
-reg[5:0] Ram_VCounter;
-reg[5:0] Ram_HCounter;
+reg[ 5: 0 ] Ram_VCounter;
+reg[ 5: 0 ] Ram_HCounter;
 reg state;
 `define idle 		0;
 `define loading 	1;
 reg VSyncPrev;
 
 initial begin
-	state <= `loading;
-	Ram_Enable <= 0;
-	Ram_VCounter <= 2;
-	Ram_HCounter <= 2;
-	Ram_Data <= 0;
+    state <= `loading;
+    Ram_Enable <= 0;
+    Ram_VCounter <= 2;
+    Ram_HCounter <= 2;
+    Ram_Data <= 0;
 end
 
 
 
-always @(posedge CLK)
-begin
-	if (VSync == 1) begin
-		Ram_Enable <= 0;
-		Ram_VCounter <= 2;
-		Ram_HCounter <= 2;
-		Ram_Data <= newChar;
-	end else begin
-		Ram_Enable <= 1;
-		Ram_HCounter <= Ram_HCounter + 1;
-		if(Ram_HCounter > 55) begin
-			Ram_VCounter <= Ram_VCounter + 1; 
-			Ram_HCounter <= 2;
-			  if (Ram_VCounter > 30) begin 
-				Ram_Enable <= 0;
-				Ram_VCounter <= 2;
-			end
-			end
-			Ram_Address[5:0]  <= Ram_HCounter[5:0];
-			Ram_Address[11:6] <= Ram_VCounter[5:0];
-	 
-		
-	end
+always @( posedge CLK ) begin
+    if ( VSync == 1 ) begin
+        Ram_Enable <= 0;
+        Ram_VCounter <= 2;
+        Ram_HCounter <= 2;
+        Ram_Data <= newChar;
+    end
+    else begin
+        Ram_Enable <= 1;
+        Ram_HCounter <= Ram_HCounter + 1;
+        if ( Ram_HCounter > 55 ) begin
+            Ram_VCounter <= Ram_VCounter + 1;
+            Ram_HCounter <= 2;
+            if ( Ram_VCounter > 30 ) begin
+                Ram_Enable <= 0;
+                Ram_VCounter <= 2;
+            end
+        end
+        Ram_Address[ 5: 0 ] <= Ram_HCounter[ 5: 0 ];
+        Ram_Address[ 11: 6 ] <= Ram_VCounter[ 5: 0 ];
+
+
+    end
 end
 endmodule
 
- 
+
