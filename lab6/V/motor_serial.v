744d11f5ccb059614105e558cb3574a4c51e3102
Thomas Russell Murphy <thomas.russell.murphy@case.edu>
2014-04-27 22:15:04 -0400

    Formatted Motor Controller Module

--- lab6/V/motor_serial.v
+++ lab6/V/motor_serial.v
@@ -1,4 +1,4 @@
-module motor_serial( sclk,ast_sink_data, ast_sink_valid, ast_sink_error, outh, outl);
+module motor_serial( sclk, ast_sink_data, ast_sink_valid, ast_sink_error, outh, outl );
 
 input wire sclk;
 input wire [ 11: 0 ] ast_sink_data; // 12 bit sample, 2's complement
@@ -9,39 +9,34 @@ output wire outl; // active low
 
 //Internal variables
 reg [ 14: 0 ] count; // 15 bits gives about 32.768 Hz
-reg [ 11: 0 ] goal; 
+reg [ 11: 0 ] goal;
 
 
 assign outl = ~outh;
 
-always @( posedge sclk ) 
-begin
+always @( posedge sclk ) begin
 
-	if (ast_sink_valid)
-		begin
-		count <= count + 1'b1;
-		
-		if ( count == 1'b0 )
-			begin
-			goal <= ast_sink_data;
-			end  // Only update the goal at zero count to avoid glitches
-			
-		if ( count <= goal )
-			begin
-			outh <= 1;
-			end
-			
-		if (count >= 20_000) // For 20 Mhz clk
-			begin
-			count = 0;
-			end
-    
-		else 
-			begin
-        outh <= 0;
-			end
-	 end
+    if ( ast_sink_valid ) begin
+        count <= count + 1'b1;
+
+        if ( count == 1'b0 ) begin
+            goal <= ast_sink_data;
+        end  // Only update the goal at zero count to avoid glitches
+
+        if ( count <= goal ) begin
+            outh <= 1;
+        end
+
+        if ( count >= 20_000 )  // For 20 Mhz clk
+        begin
+            count = 0;
+        end
+
+        else begin
+            outh <= 0;
+        end
+    end
 end
 endmodule
- 
-					
+
+
