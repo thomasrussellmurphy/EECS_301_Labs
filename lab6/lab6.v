25778d771aabf965cec6816ee45365322c04f527
Thomas Russell Murphy <thomas.russell.murphy@case.edu>
2014-05-05 23:01:57 -0400

    Removed part of Terasic copyright notice

--- lab6/lab6.v
+++ lab6/lab6.v
@@ -1,4 +1,4 @@
-// --------------------------------------------------------------------
+// Notice from Template
 // Copyright (c) 2009 by Terasic Technologies Inc.
 //
 // Permission:
@@ -7,27 +7,6 @@
 // in synthesis for all Terasic Development Boards and Altera Development
 // Kits made by Terasic. Other use of this code, including the selling,
 // duplication, or modification of any portion is strictly prohibited.
-//
-// Disclaimer:
-//
-// This VHDL/Verilog or C/C++ source code is intended as a design reference
-// which illustrates how these types of functions can be implemented.
-// It is the user's responsibility to verify their design for
-// consistency and functionality through the use of formal
-// verification methods. Terasic provides no warranty regarding the use
-// or functionality of this code.
-//
-// --------------------------------------------------------------------
-//
-// Terasic Technologies Inc
-// 356 Fu-Shin E. Rd Sec. 1. JhuBei City,
-// HsinChu County, Taiwan
-// 302
-//
-// web: http: // www.terasic.com/
-// email: support@terasic.com
-// --------------------------------------------------------------------
-
 
 module lab6
        (
